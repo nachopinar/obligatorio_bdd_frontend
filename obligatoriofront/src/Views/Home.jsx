0062142ad83f7d7ca3b86aab9e3ab5cca5c73d0f
nachopinar <136358447+nachopinar@users.noreply.github.com>
2024-11-12 13:03:23 -0300

    .

---
+++ obligatoriofront/src/Views/Home.jsx
@@ -0,0 +1,12 @@
+
+import React, { useState, useEffect } from "react";
+
+const response = await fetch("http://127.0.0.1:5000/actividad", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const data = await response.json();
+console.log(data);
